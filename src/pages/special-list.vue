bc9a8259f83102786c746e81bd7c6afc4db54804
caos <caos@a.com>
2018-09-07 18:15:30 +0800

    '专题修改删除接口'

--- src/pages/special-list.vue
+++ src/pages/special-list.vue
@@ -7,10 +7,16 @@
         <el-button size="small" type="primary" @click="search">查询</el-button>
         <el-button size="small" type="warning" @click="reset">重置</el-button>
       </div>
-      <listHandle :checkData="checkData" @refresh="refresh" :show-soldout='showSoldout'></listHandle>
+      <!-- <listHandle :checkData="checkData" @refresh="refresh" :show-soldout='showSoldout'></listHandle> -->
+      <div class="button-wrap">
+        <el-button type="primary" size="small" @click="handleFn('add')">新增</el-button>
+        <el-button type="danger" size="small" @click="handleFn('delete')" :disabled="checkData.length<1">删除</el-button>
+      </div>
     </div>
     <el-table ref="multipleTable" header-cell-class-name="tableHeader" :data="tableData" v-loading="loading" element-loading-text="拼命加载中" border stripe @selection-change="handleSelectionChange">
       <el-table-column type="selection" width="55" align="center" label-class-name="checkLabel">
+      </el-table-column>
+      <el-table-column prop="id" label="编号" align="center" min-width="60" show-overflow-tooltip>
       </el-table-column>
       <el-table-column prop="title" label="标题" align="center" min-width="180" show-overflow-tooltip>
       </el-table-column>
@@ -29,15 +35,11 @@
         </template>
       </el-table-column>
     </el-table>
-    <!-- <div class="page-wrap">
-      <el-pagination background layout="total,sizes, prev, pager, next, jumper" :total="pageInfo.totalElements" :current-page="pageInfo.currentPage" :page-sizes="pageSizes" @current-change="currentChange" @size-change="sizeChange">
-      </el-pagination>
-    </div> -->
     <page ref="pageInfo" :pageInfo="pageInfo" @sizeChange="sizeChange" @currentChange="currentChange"></page>
   </div>
 </template>
 <script>
-import { api } from '@/utils'
+import { api, confirm } from '@/utils'
 import { picker, listHandle, page } from '@/components'
 export default {
   data () {
@@ -66,6 +68,61 @@ export default {
   methods: {
     refresh () {
       this.getData(this.params)
+    },
+    handleFn (type) {
+      let { name } = this.$route
+      let path = name.split('-')[0]
+      if (type === 'add') {
+        this.toState({ name: path, params: { type: 'add' } })
+      } else {
+        let url
+        let txt
+        if (type === 'delete') {
+          url = api[`${path}Delete`]
+          txt = '删除'
+        }
+        confirm(`您确定将选择的内容${txt}吗？`, '提示').then(() => {
+          this.checkIds = []
+          for (let item = 0; item < this.checkData.length; item++) {
+            this.checkIds.push(this.checkData[item].id)
+          }
+          if (this.checkIds.length > 0) {
+            this.$fly.post(url, this.checkIds).then(data => {
+              let { allowIds, notallowIds } = data
+              if (allowIds.length > 0 && notallowIds.length > 0) {
+                this.$message({
+                  message: `已删除的编号：${allowIds}\n未删除的编号：${notallowIds}`,
+                  duration: 2000,
+                  type: 'success'
+                })
+              } else {
+                if (allowIds.length > 0) {
+                  this.$message({
+                    message: `已删除的编号：${allowIds}`,
+                    duration: 2000,
+                    type: 'success'
+                  })
+                } else if (notallowIds.length > 0) {
+                  console.log('notallowIds')
+                  this.$message({
+                    message: `未删除的编号：${notallowIds}`,
+                    duration: 2000,
+                    type: 'info'
+                  })
+                }
+              }
+              this.getData(this.params)
+            }).catch((err) => {
+              console.log(err)
+              this.$message({
+                message: `${txt}失败`,
+                duration: 2000,
+                type: 'error'
+              })
+            })
+          }
+        })
+      }
     },
     handleSelectionChange (e) {
       this.checkData = []
@@ -121,7 +178,6 @@ export default {
     this.getData(this.params)
   },
   mounted () {
-    console.log(this.$refs.pageInfo.pageSizes[0])
     this.params.size = this.$refs.pageInfo.pageSizes[0]
   }
 }
